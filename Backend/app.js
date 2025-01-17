fd4fe2c0f3048accf2d767e79f1f5744ee223b10
seifeldin15 <seifeldin02@outlook.com>
2025-01-17 06:44:57 +0200

    better logging for db

--- Backend/app.js
+++ Backend/app.js
@@ -15,10 +15,26 @@ app.use(
     cors({
         credentials: true,
         origin: ["http://localhost:3000", "http://localhost:5173"]
-
-        
     })
 )
+
+mongoose 
+.connect(mongoDBCONNECTION)
+.then(()=> {
+    app.listen(PORT, () => {
+        console.log(`app running on port: ${PORT}`);
+    });
+    console.log("connected to db");    
+})
+.catch((error)=> {
+    console.error("MongoDB connection error:", error);
+    process.exit(1);  // Exit process with failure
+});
+
+app.use((err, req, res, next) => {
+console.error(err.stack);
+res.status(500).json({ message: 'Something went wrong!' });
+});
 
 
 app.use(express.json());
@@ -36,20 +52,3 @@ app.use('/api/auth', AuthRoutes);
 app.use('/api/venues', VenueRoutes);
 app.use('/api/favorites', FavoriteRoutes);
 
-mongoose 
-    .connect(mongoDBCONNECTION)
-    .then(()=> {
-        app.listen(PORT, () => {
-            console.log(`app running on port: ${PORT}`);
-        });
-        console.log("connected to db");    
-    })
-    .catch((error)=> {
-        console.error("MongoDB connection error:", error);
-        process.exit(1);  // Exit process with failure
-    });
-
-app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).json({ message: 'Something went wrong!' });
-});
